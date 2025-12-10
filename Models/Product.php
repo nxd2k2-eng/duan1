bcbfde3b360a9dabd041d8bdbd0353e45f8c0d95
Bach-Trach <tn858549@gmail.com>
2025-12-10 08:32:28 +0700

    [Model] - product - hiển thị tất cả sản phẩm và từng sản phẩm

--- Models/Product.php
+++ Models/Product.php
@@ -1,9 +1,11 @@
 <?php
 
-class Product {
+class Product
+{
     private $connection;
 
-    public function __construct($dbConnection) {
+    public function __construct($dbConnection)
+    {
         $this->connection = $dbConnection;
     }
 
@@ -15,36 +17,30 @@ class Product {
      * @param string $keyword Từ khóa tìm kiếm
      * @return array Danh sách sản phẩm
      */
-    public function getAllProducts($page = 1, $limit = 10, $keyword = '', $active = null, $softDate = 'DESC') {
+    public function getAllProducts($page = 1, $limit = 10, $keyword = '', $active = null, $softDate = 'DESC')
+    {
         $offset = ($page - 1) * $limit;
         $search = '';
         if ($active !== null) {
-            $search .= " WHERE `active` = :active ";
+            $search .= " AND `active` = :active ";
         }
         if ($keyword !== '') {
-            $search = "AND `name` LIKE  '%$keyword%' or `description` LIKE '%$keyword%' ";
+            $search = "AND `name` LIKE '%$keyword%' or `description` LIKE '%$keyword%' ";
         }
-        
+
         if ($softDate !== null) {
             $search .= " ORDER BY `created_at` $softDate ";
         }
-        $query = "SELECT * FROM `products` $search  LIMIT :limit OFFSET :offset";
+       
+        $query = "SELECT * FROM `products` WHERE 1 $search  LIMIT :limit OFFSET :offset";
+       
         $stmt = $this->connection->prepare($query);
+        if ($active !== null) {
+            $stmt->bindParam(':active', $active, PDO::PARAM_INT);
+        }
+        $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+        $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function getOneProduct($id, $active = null) {
-        if ($active !== null) {
-            $search = " AND `active` = :active ";
-        } else {
-            $search = '';
-        }
-
-        $query = "SELECT * FROM `products` WHERE `product_id` = :id";
-        $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
--- router.php
+++ router.php
@@ -3,9 +3,10 @@
 require_once 'Models/Product.php';
 $productModel = new Product($connection);
 $productList = $productModel->getAllProducts(1, 10);
+var_dump($productList);
 
-$product = $productModel->getOneProduct(1);
-var_dump($product);
+// $product = $productModel->getOneProduct(1);
+// var_dump($product);
 
-$product = $productModel->getOneProduct(2);
-var_dump($product);
+// $product = $productModel->getOneProduct(2);
+// var_dump($product);
