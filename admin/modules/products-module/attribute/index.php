06064bc36f14057a0b710aa3c44eaf3b3f9bd297
hoaian33333 <nguyenangaming61@gmail.com>
2025-12-17 00:47:50 +0700

    PHP-admin

---
+++ admin/modules/products-module/attribute/index.php
@@ -0,0 +1,66 @@
+<button type="button" class="btn btn-outline-primary mb-3">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle"
+        viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+        <path
+            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+    </svg>
+    Thêm mới
+</button>
+<div class="card"></div>
+<div class="card-header">
+    Danh sách thuộc tính
+</div>
+<div class="card-body">
+    <div class="row">
+        <form action="">
+            <div class="col-md-4">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Tìm kiếm danh mục">
+                    <button type="submit" class="btn btn-primary">Tìm kiếm</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<div class="table-reponsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col" style="width: 70%;">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Trạng thái</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="switchCheckChecked" checked>
+                    </div>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-outline-primary">Sửa</button>
+                    <button type="button" class="btn btn-outline-danger">Xóa</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<div class="card-footer">
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+    </nav>
+</div>
+</div>
