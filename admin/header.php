65be4c5f8282cb9d12bdd4f365e9eb58fdfc3b23
nxd2k2-eng <nxd2k2@gmail.com>
2025-12-17 01:58:12 +0700

    Fix CSS

--- admin/header.php
+++ admin/header.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Flexy Free Bootstrap Admin Template by WrapPixel</title>
     <link rel="shortcut icon" type="image/png" href="/admin/assets/images/logos/favicon.png" />
-    <link rel="stylesheet" href="admin/assets/css/styles.min.css" />
+    <link rel="stylesheet" href="/admin/assets/css/styles.min.css" />
 </head>
     
 <body>
     <!--  Body Wrapper -->
     <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
-        data-sidebar-position="fixed" data-header-position="fixed"></div>
+        data-sidebar-position="fixed" data-header-position="fixed">
