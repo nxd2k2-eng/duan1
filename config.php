e9dee479574c6a8cfc695cd6caa8ae3e9e4c7299
nxd2k2-eng <nxd2k2@gmail.com>
2025-12-17 01:43:02 +0700

    PHP-Full chức năng

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'onehost-wphn092505.000nethost.com');
-define('DB_USER', 'cidkghirhosting_lumos');
-define('DB_PASS', '.1S#$SdUjs&BMaW');
-define('DB_NAME', 'cidkghirhosting_csdl_lumos');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'mysql');
+define('DB_NAME', 'duan1');
 define('DB_PORT', 3306);
